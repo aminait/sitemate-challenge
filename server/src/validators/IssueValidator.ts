9e37b1ac83bdd1e67ec7d1f3f2ee849915e7706f
Amina Ait <aminait@outlook.com>
2024-09-03 19:41:07 +0400

    Add create issue validation

---
+++ server/src/validators/IssueValidator.ts
@@ -0,0 +1,10 @@
+import Joi from 'joi';
+
+export const createIssueSchema = Joi.object({
+  title: Joi.string().required().messages({
+    'string.empty': 'Title is required',
+  }),
+  description: Joi.string().required().messages({
+    'string.empty': 'Description is required',
+  }),
+});
