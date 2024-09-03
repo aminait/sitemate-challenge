9b9e5cd999f1e20ed6f2beb1519dfb551eb7b94e
Amina Ait <aminait@outlook.com>
2024-09-03 19:38:22 +0400

    Create issue service

---
+++ server/src/services/IssueService.ts
@@ -0,0 +1,22 @@
+import { IssueModel, IIssue } from 'src/models';
+
+// TODO add validation
+
+export class IssueService {
+  public async createIssue(title: string, description: string): Promise<IIssue> {
+    const issue = new IssueModel({ title, description });
+    return issue.save();
+  }
+
+  public async readIssue(id: string): Promise<IIssue | null> {
+    return IssueModel.findById(id);
+  }
+
+  public async updateIssue(id: string, updateData: Partial<IIssue>): Promise<IIssue | null> {
+    return IssueModel.findByIdAndUpdate(id, updateData, { new: true }); // to return the entire document with the latest change
+  }
+
+  public async deleteIssue(id: string): Promise<IIssue | null> {
+    return IssueModel.findByIdAndDelete(id);
+  }
+}
